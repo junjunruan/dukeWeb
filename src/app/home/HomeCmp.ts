55e93487203f929ef7ec935b2726b133376d036c
Junjun Ruan <junjunruan@gmail.com>
2016-12-01 16:42:54 -0500

    add background image to home page

--- src/app/home/HomeCmp.ts
+++ src/app/home/HomeCmp.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'home-cmp',
-    templateUrl: 'Home.html'
+    templateUrl: 'Home.html',
+    styleUrls: ['Home.css']
 })
 
 export class HomeCmp {}
