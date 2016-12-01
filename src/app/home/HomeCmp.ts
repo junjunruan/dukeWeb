e007a8d87ca0e25a8232cb3061edf0d307186e55
Junjun Ruan <junjunruan@gmail.com>
2016-12-01 09:37:15 -0500

    update tslint rules

---
+++ src/app/home/HomeCmp.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'home-cmp',
+    templateUrl: 'app/Home.html'
+})
+
+export class HomeCmp {}
---
+++ src/app/resume/ResumeCmp.ts
@@ -0,0 +1,3 @@
+/**
+ * Created by junjun on 11/30/16.
+ */
