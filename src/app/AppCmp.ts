22b7ffa920145a099fbc5f6aa62c5b31ffb9e59b
Junjun Ruan <junjunruan@gmail.com>
2016-12-01 09:39:44 -0500

    add app component

--- src/app/AppCmp.ts
+++ src/app/AppCmp.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { RoutesPathName } from './RoutesPathName';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-cmp',
   templateUrl: 'App.html',
   styleUrls: ['App.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  private _home: string = '/' + RoutesPathName.home;
+  private _resume: string = '/' + RoutesPathName.resume;
+  private _portfolio: string = '/' + RoutesPathName.portfolio;
+  private _about: string = '/' + RoutesPathName.about;
+  private _contact: string = '/' + RoutesPathName.contact;
+
+  private title: string = 'app works!';
+
 }
+
