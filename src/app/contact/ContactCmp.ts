c9610de3dbbde470bee39ba53d5e2b990a2d4157
Junjun Ruan <junjunruan@gmail.com>
2016-12-01 09:41:15 -0500

    add contact component

--- src/app/contact/ContactCmp.ts
+++ src/app/contact/ContactCmp.ts
@@ -1,3 +1,10 @@
-/**
- * Created by junjun on 11/30/16.
- */
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'contact-cmp',
+    template: `contact-page`
+})
+
+export class ContactCmp {}
+
+
